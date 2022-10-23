49c152035eab783b3913c12b2061c56f5fb6b316
lauratrandafir2001 <92182919+lauratrandafir2001@users.noreply.github.com>
2022-10-24 00:35:56 +0300

    Add files via upload

---
+++ requests.h
@@ -0,0 +1,22 @@
+#ifndef _REQUESTS_
+#define _REQUESTS_
+
+// computes and returns a GET request string (query_params
+// and cookies can be set to NULL if not needed)
+char *compute_get_request(char *host, char *url, char *query_params,
+						  char **cookies, int cookies_count);
+
+// computes and returns a POST request string (cookies can be NULL if not needed)
+char *compute_post_request(char *host, char *url, char *content_type, char **body_data,
+						   int body_data_fields_count, char **cookies, int cookies_count);
+
+char *compute_post_request_with_headers(char *host, char *url, char *content_type, char **body_data,
+										int body_data_fields_count, char **cookies, int cookies_count, char *header, int headers_count);
+
+char *compute_get_request_with_headers(char *host, char *url, char *query_params,
+									   char **cookies, int cookies_count, char *header, int headers_count);
+
+char *compute_delete_request_with_headers(char *host, char *url, char *query_params,
+										  char **cookies, int cookies_count, char *token, int tokent_count);
+
+#endif
